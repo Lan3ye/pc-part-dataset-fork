4ab62cc6068f024827a822322ade7f51c07e7e8b
Janik Krause <janik.krause2002@gmail.com>
2023-11-27 11:28:05 +0100

    CPUs are now scraped with sockets. Problems: - Some sockets have enough entries for multiple pages, that doens't work   correctly yet - Sockets and their paths need to be defined by hand and are not updated   automatically

--- src/scraper.ts
+++ src/scraper.ts
@@ -42,6 +42,38 @@ const ALL_ENDPOINTS: PartType[] = [
 	'ups',
 ]
 
+type MyDictionary = Record<string, string>;
+
+// cpu-Socket cpu/#k=<number>
+const cpuSockets: MyDictionary = {
+	AM1:"#k=27",
+	AM2p: "#k=2",
+	AM3: "#k=3",
+	AM3p: "#k=4",
+	AM4: "#k=33", 
+	AM5: "#k=41",
+	FM1: "#k=20",
+	FM2: "#k=23",
+	FM2p: "#k=26",
+	G34: "#k=31",
+	LGA771: "#k=12",
+	LGA775: "#k=13",
+	LGA1150: "#k=24",
+	LGA1151: "#k=30",
+	LGA1155: "#k=14",
+	LGA1156: "#k=15",
+	LGA1200: "#k=39",
+	LGA1356: "#k=37",
+	LGA1366: "#k=16",
+	LGA1700: "#k=40",
+	LGA2011: "#k=21",
+	LGA2011_3: "#k=28",
+	LGA2066: "#k=35",
+	sTR4: "#k=36",
+	sTRX4: "#k=38"
+}
+
+
 puppeteer.use(StealthPlugin())
 
 const map = untypedMap as unknown as SerializationMap
@@ -92,80 +124,91 @@ async function scrapeInParallel(endpoints: PartType[]) {
 }
 
 async function* scrape(endpoint: PartType, page: Page): AsyncGenerator<Part[]> {
-	await page.goto(`${BASE_URL}/${endpoint}/${'#k=27'}`)
-
-	const paginationEl = await page.waitForSelector('.pagination', {
+	
+	for (const [_, socketUrl] of Object.entries(cpuSockets)) {
+		const fullUrl = `${BASE_URL}/${endpoint}/${socketUrl}`;
+		await page.goto(fullUrl, { waitUntil: 'domcontentloaded' })
+		await page.waitForNetworkIdle()
+		
+		const paginationEl = await page.waitForSelector('.pagination', {
 		timeout: 5000,
-	})
-
-	// NOTE: We are banging paginationEl because Page.waitForSelector()
-	// only returns null when using option `hidden: true`, which we
-	// are not using.
-	// See: https://pptr.dev/api/puppeteer.page.waitforselector#parameters
-	const numPages = await paginationEl!.$eval('li:last-child', (el) =>
-		parseInt(el.innerText)
-	)
-
-	for (let currentPage = 1; currentPage <= numPages; currentPage++) {
-		const pageProducts: Part[] = []
-
-		if (currentPage > 1) {
-			await page.goto(`${BASE_URL}/${endpoint}/#page=${currentPage}`)
-			await page.waitForNetworkIdle()
-		}
-
-		const productEls = await page.$$('.tr__product')
-
-		for (const productEl of productEls) {
-			const serialized: Part = {}
-
-			serialized['name'] = await productEl.$eval(
-				'.td__name .td__nameWrapper > p',
-				(p) => p.innerText
-			)
-
-			const priceText = await productEl.$eval(
-				'.td__price',
-				(td) => td.textContent
-			)
-
-			if (priceText == null || priceText.trim() === '')
-				serialized['price'] = null
-			else serialized['price'] = serializeNumber(priceText)
-
-			const specs = await productEl.$$('td.td__spec')
-
-			for (const spec of specs) {
-				const specName = await spec.$eval('.specLabel', (l) =>
-					(l as HTMLHeadingElement).innerText.trim()
-				)
-				const mapped = map[endpoint][specName]
-
-				if (typeof mapped === 'undefined')
-					throw new Error(`No mapping found for spec '${specName}'`)
-
-				const [snakeSpecName, mappedSpecSerializationType] = mapped
-
-				const specValue = await spec.evaluate((s) => s.innerText)
-
-				if (specValue.trim() === '') {
-					serialized[snakeSpecName] = null
-				} else if (mappedSpecSerializationType === 'custom') {
-					serialized[snakeSpecName] =
-						customSerializers[endpoint]![snakeSpecName]!(specValue)
-				} else {
-					serialized[snakeSpecName] = genericSerialize(
-						specValue,
-						mappedSpecSerializationType
-					)
-				}
+		})
+		
+		// NOTE: We are banging paginationEl because Page.waitForSelector()
+		// only returns null when using option `hidden: true`, which we
+		// are not using.
+		// See: https://pptr.dev/api/puppeteer.page.waitforselector#parameters
+		const numPages = await paginationEl!.$eval('li:last-child', (el) =>
+			parseInt(el.innerText)
+		)
+		
+		for (let currentPage = 1; currentPage <= numPages; currentPage++) {
+			const pageProducts: Part[] = []
+	
+			if (currentPage > 1) {
+				await page.goto(`${BASE_URL}/${endpoint}/#page=${currentPage}`)
+				await page.waitForNetworkIdle()
 			}
+	
+			const productEls = await page.$$('.tr__product')
+	
+			for (const productEl of productEls) {
+				const serialized: Part = {}
+				// Exctracting socket name from Current CPU socket
+				const socketName = Object.keys(cpuSockets).find(
+					(key) => cpuSockets[key] === socketUrl
+				)
 
-			pageProducts.push(serialized)
+	
+				serialized['name'] = `${socketName} ${await productEl.$eval(
+					'.td__name .td__nameWrapper > p',
+					(p) => p.innerText
+				)}`
+	
+				const priceText = await productEl.$eval(
+					'.td__price',
+					(td) => td.textContent
+				)
+	
+				if (priceText == null || priceText.trim() === '')
+					serialized['price'] = null
+				else serialized['price'] = serializeNumber(priceText)
+	
+				const specs = await productEl.$$('td.td__spec')
+	
+				for (const spec of specs) {
+					const specName = await spec.$eval('.specLabel', (l) =>
+						(l as HTMLHeadingElement).innerText.trim()
+					)
+					const mapped = map[endpoint][specName]
+	
+					if (typeof mapped === 'undefined')
+						throw new Error(`No mapping found for spec '${specName}'`)
+	
+					const [snakeSpecName, mappedSpecSerializationType] = mapped
+	
+					const specValue = await spec.evaluate((s) => s.innerText)
+	
+					if (specValue.trim() === '') {
+						serialized[snakeSpecName] = null
+					} else if (mappedSpecSerializationType === 'custom') {
+						serialized[snakeSpecName] =
+							customSerializers[endpoint]![snakeSpecName]!(specValue)
+					} else {
+						serialized[snakeSpecName] = genericSerialize(
+							specValue,
+							mappedSpecSerializationType
+						)
+					}
+				}
+	
+				pageProducts.push(serialized)
+			}
+	
+			yield pageProducts
 		}
-
-		yield pageProducts
 	}
+	
 }
 
 const inputEndpoints = process.argv.slice(2)
